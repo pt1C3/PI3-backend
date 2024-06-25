790bdc4df30dcbdb1c7e24abe77074c342e9b5e3
pt1C3 <rafaelsilva1c3@gmail.com>
2024-06-25 18:13:54 +0100

    oopsie

--- src/routes/produtosRouter.js
+++ src/routes/produtosRouter.js
@@ -19,8 +19,9 @@ const upload = multer({ storage: storage }); //Cria a função/middleware respon
 
 router.get('/', controller.produtos_min_list);
 router.post('/create', /*upload.single('foto'),*/ controller.produtos_add); //O middleware é chamado antes do controller
-router.get('/:id', controller.single_product);
 router.get('/soum', controller.apenasum);
+
+router.get('/:id', controller.single_product);
 
 /*
 router.put('/update/:id', upload.single('foto'), controller.filme_update); //O middleware é chamado antes do controller
